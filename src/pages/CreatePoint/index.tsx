c56d023ec9fe65fe780c5f3cc1a8f544473d7ab0
Jonatan Paes <jonatan.paes@itst.com.br>
2020-06-07 11:53:30 -0300

    carregar localização atual do usuario

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -27,9 +27,19 @@ const CreatePoint = () => {
 	const [ ufs, setUfs ] = useState<string[]>([]);
 	const [ cities, setCities ] = useState<string[]>([]);
 
+	const [ initialPosition, setInitialPosition ] = useState<[number, number]>([ 0, 0 ]);
+
 	const [ selectedUf, setSelectedUf ] = useState('0');
 	const [ selectedCity, setSelectedCity ] = useState('0');
 	const [ selectedPosition, setSelectedPosition ] = useState<[number, number]>([ 0, 0 ]);
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			const { latitude, longitude } = position.coords;
+
+			setInitialPosition([ latitude, longitude ]);
+		});
+	}, []);
 
 	useEffect(() => {
 		api.get('items').then((response) => {
@@ -125,7 +135,7 @@ const CreatePoint = () => {
 						<span>Selecione o endereço no mapa</span>
 					</legend>
 
-					<Map center={[ -23.1014801, -48.6066484 ]} zoom={15} onClick={handleMapClick}>
+					<Map center={initialPosition} zoom={15} onClick={handleMapClick}>
 						<TileLayer
 							attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
 							url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
